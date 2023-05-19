6f759093808e8660225861b9e2feb0fde78bbe20
Catalin Ta <user@skillzl.me>
2023-05-19 15:31:38 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,21 +17,21 @@ return (
     <br />
     <br />
     <div className="mb-4">
-      <img src="https://japi.rest/discord/v1/user/1049708735821516830/avatar?size=256" />
+      <img src="https://japi.rest/discord/v1/user/565960314970177556/avatar?size=256" />
       <br />
-      <h2 className="text-3xl font-medium mb-1">chrlwv.tech</h2>
+      <h2 className="text-3xl font-medium mb-1">chrlwv.tech (offline)</h2>
       <div>
         <div className="text-secondary font-mono">
-          A multipurpose discord client written in <span
+          (Discontinued) A multipurpose discord client written in <span
             className="font-bold text-yellow-200 text-lg">javascript</span>.
           Used to be a freelance start-up project but now it's a public client for all around the world users.
         </div>
       </div>
     </div>
     <div>
-      <a href='https://discord.com/api/oauth2/authorize?client_id=1049708735821516830&permissions=8&scope=bot%20applications.commands'><button
+      <a href='https://skillzl.me'><button
           className="px-4 py-2 rounded-l-xl text-white m-0 bg-green-500 hover:bg-green-600 transition">Invite</button></a>
-      <a href='https://discord.gg/frJSrhwwer'><button
+      <a href='https://skillzl.me'><button
           className="px-4 py-2 rounded-r-xl text-black bg-neutral-50 hover:bg-neutral-300 transition">Support</button></a>
     </div>
   </Container>
